b5dc6de72a8654b39e8371e81ffd0064a832ccff
kingdaro <kingdaro@gmail.com>
2017-09-30 01:09:39 -0400

    added a context menu for the tray

--- src/main/tray.js
+++ src/main/tray.js
@@ -1,14 +1,20 @@
 // @ts-check
-import { Tray } from 'electron'
+import { app, Tray, Menu } from 'electron'
 import { resolve } from 'path'
-import { toggleWindow } from './window'
+import { toggleWindow, showWindow } from './window'
 
 /** @type {Tray} */
 let tray
 
 export function createTray() {
-  tray = new Tray(resolve(__static, 'icon.png'))
+  const menu = Menu.buildFromTemplate([
+    { label: 'Show', click: showWindow },
+    { type: 'separator' },
+    { label: 'Exit', click: () => app.quit() },
+  ])
 
+  tray = new Tray(resolve(__static, 'icon.png'))
   tray.on('click', toggleWindow)
   tray.on('double-click', toggleWindow)
+  tray.setContextMenu(menu)
 }
