0e3f3210e2789d70facaed35925d435dd4ac9aa0
kingdaro <kingdaro@gmail.com>
2018-05-06 04:15:36 -0400

    move default dongers to a separate file

--- src/configStore.ts
+++ src/configStore.ts
@@ -1,36 +1,21 @@
-import * as ConfigStore from 'configstore'
-import { Donger } from './donger'
+import ConfigStore from "configstore"
+import defaultDongers from "./defaultDongers"
+import { Donger } from "./donger"
 
-const meta = require('../package.json')
+const meta = require("../package.json")
 
-const defaults = {
-  dongers: [
-    new Donger('shrug', String.raw`¯\_(ツ)_/¯`),
-    new Donger('shrug (markdown)', String.raw`¯\\\_(ツ)\_/¯`),
-    new Donger('flower', `(◕‿◕✿)`),
-    new Donger('peace', `(⌣‿⌣✿)`),
-    new Donger('give', `༼ つ ◕_◕ ༽つ`),
-    new Donger('OG', `ヽ༼ຈل͜ຈ༽ﾉ`),
-    new Donger('lenny', `( ͡° ͜ʖ ͡°)`),
-    new Donger('dance', `ᕕ( ᐛ )ᕗ`),
-    new Donger('stars', `(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧`),
-    new Donger('shige', `(´・◡ ・｀)`),
-    new Donger('lenny-drugs', '( ͡☉ ͜ʖ ͡☉)'),
-  ],
-}
-
-const store = new ConfigStore(meta.name, defaults)
+const store = new ConfigStore(meta.name, defaultDongers())
 
 export function getDongers(): Donger[] {
-  return store.get('dongers')
+  return store.get("dongers")
 }
 
 export function addDonger(donger: Donger) {
-  store.set('dongers', getDongers().concat(donger))
+  store.set("dongers", getDongers().concat(donger))
 }
 
 export function removeDonger(id: string) {
-  store.set('dongers', getDongers().filter(d => d.id !== id))
+  store.set("dongers", getDongers().filter(d => d.id !== id))
 }
 
 export function updateLastUsed(id: string, date: number) {
@@ -38,6 +23,6 @@ export function updateLastUsed(id: string, date: number) {
   const donger = dongerList.find(d => d.id === id)
   if (donger) {
     donger.dateLastUsed = date
-    store.set('dongers', dongerList)
+    store.set("dongers", dongerList)
   }
 }
